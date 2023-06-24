88f74d66c2f6bc819480cccd5156fde1194332e7
Vanessa Pereira <vnspereira11@gmail.com>
2023-06-24 20:21:07 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { SignIn } from "./pages/SignIn";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SignIn />
+  </React.StrictMode>
+);
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,7 @@
+export function SignIn() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
