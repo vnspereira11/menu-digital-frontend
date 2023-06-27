13b10826422a612f7b172eea093a125ac4f049aa
Vanessa Pereira <vnspereira11@gmail.com>
2023-06-27 11:13:47 -0300

    Update Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,12 @@
 import { Container } from "./styles";
 
-export function Button({ title, loading = false }) {
+export function Button({ title, loading = false, ...rest }) {
   return (
     <Container
       type="button"
       disabled={loading}
       className={loading ? "button-loading" : "button-active"}
+      {...rest}
     >
       {title}
     </Container>
