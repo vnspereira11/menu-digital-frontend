bde68b6f081ed1f61c779d178e95bb32fa1ba2fa
Vanessa Pereira <vnspereira11@gmail.com>
2023-08-10 17:38:11 -0300

    Implement meal fetching and Card loading

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
 import { FiSearch } from "react-icons/fi";
+
+import { api } from "../../services/api";
+
 import { Container } from "./styles";
 
 import { Header } from "../../components/Header";
@@ -9,7 +13,30 @@ import { Carousel } from "../../components/Carousel";
 import { Card } from "../../components/Card";
 import { Footer } from "../../components/Footer";
 
-export function Home({ isAdmin, ...rest }) {
+export function Home({ ...rest }) {
+  const [search, setSearch] = useState("");
+  const [meals, setMeals] = useState([]);
+  const [desserts, setDesserts] = useState([]);
+  const [beverages, setBeverages] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      try {
+        const response = await api.get(`/meals?search=${search}`);
+        console.log(response);
+        setMeals(response.data.filter((meal) => meal.category === "meal"));
+        setDesserts(
+          response.data.filter((meal) => meal.category === "dessert")
+        );
+        setBeverages(
+          response.data.filter((meal) => meal.category === "beverage")
+        );
+      } catch {}
+    }
+
+    fetchMeals();
+  }, [search]);
+
   return (
     <Container {...rest}>
       <Header>
@@ -17,78 +44,38 @@ export function Home({ isAdmin, ...rest }) {
           icon={FiSearch}
           type="search"
           placeholder="Busque por pratos ou ingredientes"
+          onChange={(e) => setSearch(e.target.value)}
         />
       </Header>
       <main>
         <Banner />
-        <Section title="Refeições">
-          <Carousel>
-            <Card
-              className="is-admin"
-              isAdmin
-              data={{ title: "Salada Ravanello >", price: "R$ 49,97" }}
-            />
-            <Card
-              className="is-admin"
-              isAdmin
-              data={{ title: "Salada Ravanello >", price: "R$ 49,97" }}
-            />
-            <Card
-              className="is-admin"
-              isAdmin
-              data={{ title: "Salada Ravanello >", price: "R$ 49,97" }}
-            />
-            <Card
-              className="is-admin"
-              isAdmin
-              data={{ title: "Salada Ravanello >", price: "R$ 49,97" }}
-            />
-            <Card
-              className="is-admin"
-              isAdmin
-              data={{ title: "Salada Ravanello >", price: "R$ 49,97" }}
-            />
-            <Card
-              className="is-admin"
-              isAdmin
-              data={{ title: "Salada Ravanello >", price: "R$ 49,97" }}
-            />
-            <Card
-              className="is-admin"
-              isAdmin
-              data={{ title: "Salada Ravanello >", price: "R$ 49,97" }}
-            />
-            <Card
-              className="is-admin"
-              isAdmin
-              data={{ title: "Salada Ravanello >", price: "R$ 49,97" }}
-            />
-          </Carousel>
-        </Section>
-        <Section title="Sobremesas">
-          <Carousel>
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-          </Carousel>
-        </Section>
-        <Section title="Bebidas">
-          <Carousel>
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-            <Card data={{ title: "Salada Ravanello >", price: "R$ 49,97" }} />
-          </Carousel>
-        </Section>
+        {meals.length > 0 && (
+          <Section title="Refeições">
+            <Carousel>
+              {meals.map((meal) => (
+                <Card key={String(meal.id)} data={meal} />
+              ))}
+            </Carousel>
+          </Section>
+        )}
+        {desserts.length > 0 && (
+          <Section title="Sobremesas">
+            <Carousel>
+              {desserts.map((meal) => (
+                <Card key={String(meal.id)} data={meal} />
+              ))}
+            </Carousel>
+          </Section>
+        )}
+        {beverages.length > 0 && (
+          <Section title="Bebidas">
+            <Carousel>
+              {beverages.map((meal) => (
+                <Card key={String(meal.id)} data={meal} />
+              ))}
+            </Carousel>
+          </Section>
+        )}
       </main>
       <Footer />
     </Container>
