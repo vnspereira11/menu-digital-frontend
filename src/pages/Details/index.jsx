8703e0591c5b46312fb3fc9dc11bd62db541f091
Vanessa Pereira <vnspereira11@gmail.com>
2023-08-13 20:20:20 -0300

    Refactor Details page: show meal data and image

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,8 +5,8 @@ import { api } from "../../services/api";
 
 import { FiSearch } from "react-icons/fi";
 import { PiReceiptBold } from "react-icons/pi";
+import mealPlaceHolder from "../../assets/meal_placeholder.png";
 
-import mealImg from "../../assets/ravanello300.svg";
 import {
   Container,
   Content,
@@ -28,6 +28,11 @@ export function Details() {
   const [data, setData] = useState(null);
   const params = useParams();
 
+  const mealImage =
+    data && data.image
+      ? `${api.defaults.baseURL}/files/${data.image}`
+      : mealPlaceHolder;
+
   useEffect(() => {
     async function fetchMeal() {
       const response = await api.get(`/meals/${params.id}`);
@@ -47,33 +52,35 @@ export function Details() {
       </Header>
       <main>
         <BackButton title="voltar" to="/" />
-        <Content>
-          <MealImage>
-            <img src={mealImg} alt="" />
-          </MealImage>
-          <MealData>
-            <h1>{data.name}</h1>
-            <p>{data.description}</p>
-            {data.ingredients && (
-              <MealIngredients>
-                {data.ingredients.map((ingredient) => (
-                  <Ingredient
-                    key={String(ingredient.id)}
-                    title={ingredient.name}
-                  />
-                ))}
-              </MealIngredients>
-            )}
-            <AddOrder>
-              <Stepper count="01" />
-              <OrderButton
-                icon={PiReceiptBold}
-                title="Incluir R$ "
-                amount={25}
-              />
-            </AddOrder>
-          </MealData>
-        </Content>
+        {data && (
+          <Content>
+            <MealImage>
+              <img src={mealImage} alt="" />
+            </MealImage>
+            <MealData>
+              <h1>{data.name}</h1>
+              <p>{data.description}</p>
+              {data.ingredients && (
+                <MealIngredients>
+                  {data.ingredients.map((ingredient) => (
+                    <Ingredient
+                      key={String(ingredient.id)}
+                      title={ingredient.name}
+                    />
+                  ))}
+                </MealIngredients>
+              )}
+              <AddOrder>
+                <Stepper count="01" />
+                <OrderButton
+                  icon={PiReceiptBold}
+                  title="Incluir R$ "
+                  amount={25}
+                />
+              </AddOrder>
+            </MealData>
+          </Content>
+        )}
       </main>
       <Footer />
     </Container>
